4da6f7d1eb29c86024e37a684d498af98072a297
Cindy-Master <2606440373@qq.com>
2026-06-08 10:00:18 +0800

    v2.6.4: 答题提交/继续按钮文字跟随自定义按键（不再写死 Enter）

--- VocabSpire/UI/QuizPanel.cs
+++ VocabSpire/UI/QuizPanel.cs
@@ -165,7 +165,7 @@ public partial class QuizPanel : Control
         mainVBox.AddChild(confirmContainer);
         _confirmButton = new Button
         {
-            Text = "  继续 (Enter)  ",
+            Text = "  继续  ",
             CustomMinimumSize = new Vector2(200, 44),
             Visible = false
         };
@@ -254,6 +254,7 @@ public partial class QuizPanel : Control
         }
 
         _feedbackLabel.Text = "";
+        _confirmButton.Text = $"  继续 ({KeyBindButton.KeyName(VocabConfig.Instance.ContinueKey)})  ";
         _confirmButton.Visible = false;
         UpdateStats();
         Visible = true;
